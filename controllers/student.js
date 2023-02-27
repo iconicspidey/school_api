ca22051b1253ff154609d7c254bff4554c91bbf7
iconicspidey <64977181+iconicspidey@users.noreply.github.com>
2023-02-27 03:58:32 +0100

    did something

--- controllers/student.js
+++ controllers/student.js
@@ -1,10 +1,15 @@
 // const person = require("random-person");
-const student = (req, res) => {
-  const array = [];
-  for (let i = 0; i < 100; i++) {
-    const random = person.person();
-    array.push(random);
+const student = async (req, res) => {
+  // const array = [];
+  // for (let i = 0; i < 100; i++) {
+  //   const random = person.person();
+  //   array.push(random);
+  // }
+  try {
+    const user = await fetch("https://random-data-api.com/api/v2/users");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
   }
-  res.status(200).json(array);
 };
 module.exports = student;
